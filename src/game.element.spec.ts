d31a10a238e1cd23cd5d682399612e7411fd906c
dblue <dannybluedesign@gmail.com>
2022-04-07 13:24:40 -0400

    update

--- src/game.element.spec.ts
+++ src/game.element.spec.ts
@@ -2,54 +2,22 @@ import { expect, fixture, html } from "@open-wc/testing";
 
 import { GoBoardElement } from "./board.element";
 import { GoGameElement } from "./game.element";
+import { GoStoneElement } from "./stone.element";
 
 customElements.define("go-game", GoGameElement);
 customElements.define("go-board", GoBoardElement);
+customElements.define("go-stone", GoStoneElement);
 
 describe(GoGameElement.name, () => {
-  describe("parseUserCoords", () => {
-    function createBoard() {
-      return fixture<GoGameElement>(html`
-        <go-game>
-          <go-board id="board"></go-board>
-        </go-game>
-      `);
-    }
+  it("should workd", async () => {
+    const el = await fixture<GoGameElement>(html`
+      <go-game>
+        <go-board>
+          <go-stone slot="D16"></go-stone>
+        </go-board>
+      </go-game>
+    `);
 
-    it("should parse user readable space to coords of first", async () => {
-      const el = await createBoard();
-
-      expect(el.parseUserCoords("A19")).to.deep.equal({
-        row: 0,
-        col: 0,
-      });
-    });
-
-    it("should parse user readable space to coords of last", async () => {
-      const el = await createBoard();
-
-      expect(el.parseUserCoords("S1")).to.deep.equal({
-        row: 18,
-        col: 18,
-      });
-    });
-
-    it("should parse user readable space to coords of topleft starpoint", async () => {
-      const el = await createBoard();
-
-      expect(el.parseUserCoords("D16")).to.deep.equal({
-        row: 3,
-        col: 3,
-      });
-    });
-
-    it("should parse user readable space to coords of bottom starpoint", async () => {
-      const el = await createBoard();
-
-      expect(el.parseUserCoords("P4")).to.deep.equal({
-        row: 15,
-        col: 15,
-      });
-    });
+    console.log(el.findGroup(el.querySelector("slot[D16]")!));
   });
 });
--- src/game.element.ts
+++ src/game.element.ts
@@ -1,20 +1,20 @@
-import { Injected } from '@joist/di';
-import { injectable } from '@joist/di/dom';
-import { attr, observable, observe } from '@joist/observable';
-import { query, queryAll } from '@joist/query';
+import { Injected } from "@joist/di";
+import { injectable } from "@joist/di/dom";
+import { attr, observable, observe } from "@joist/observable";
+import { query, queryAll } from "@joist/query";
 
-import { alphabet, BoardEvent, GoBoardElement } from './board.element';
-import { Debug } from './go.ctx';
-import { GoStoneElement, StoneColor } from './stone.element';
+import { alphabet, BoardEvent, GoBoardElement } from "./board.element";
+import { Debug } from "./go.ctx";
+import { GoStoneElement, StoneColor } from "./stone.element";
 
 @observable
 @injectable
 export class GoGameElement extends HTMLElement {
   static inject = [Debug];
 
-  @observe @attr turn: StoneColor = 'black';
+  @observe @attr turn: StoneColor = "black";
 
-  @query('#board,go-board') board!: GoBoardElement;
+  @query("#board,go-board") board!: GoBoardElement;
 
   @queryAll("go-stone[color='black']", { cache: false })
   black!: NodeListOf<GoStoneElement>;
@@ -25,54 +25,102 @@ export class GoGameElement extends HTMLElement {
   constructor(private debug: Injected<Debug>) {
     super();
 
-    this.addEventListener('goboard', (e) => {
+    this.addEventListener("goboard", (e) => {
       const evt = e as BoardEvent;
 
       const stone = new GoStoneElement();
       stone.color = this.turn;
       stone.slot = evt.space;
 
-      this.debug().log('Adding Stone', stone);
+      this.debug().log("Adding Stone", stone);
 
       this.board.appendChild(stone);
 
-      this.turn = this.turn === 'black' ? 'white' : 'black';
+      this.turn = this.turn === "black" ? "white" : "black";
       this.board.turn = this.turn;
     });
 
-    this.addEventListener('contextmenu', (e) => {
+    this.addEventListener("contextmenu", (e) => {
       if (e.target instanceof GoStoneElement && e.target.slot) {
         e.preventDefault();
 
         this.board.removeChild(e.target);
 
-        this.debug().log('Stone removed: ', e.target);
+        this.debug().log("Stone removed: ", e.target);
       }
     });
   }
 
   connectedCallback() {
     if (!this.board) {
-      throw new Error('no board found');
+      throw new Error("no board found");
     }
 
-    this.style.display = 'inline-block';
+    this.style.display = "inline-block";
 
     const debug = this.debug();
 
-    debug.log('black:', this.black.length);
-    debug.log('white:', this.white.length);
+    debug.log("black:", this.black.length);
+    debug.log("white:", this.white.length);
 
-    this.turn = this.black.length > this.white.length ? 'white' : 'black';
+    this.turn = this.black.length > this.white.length ? "white" : "black";
     this.board.turn = this.turn;
   }
 
-  parseUserCoords(space: string) {
-    const array = space.split('');
+  parseCoords(space: string) {
+    const array = space.split("");
 
     return {
-      col: alphabet.indexOf(array.shift() as string),
-      row: this.board.rows - Number(array.join('')),
+      col: array.shift() as string,
+      row: array.join(""),
     };
+  }
+
+  findGroup(stone: GoStoneElement, stones: GoStoneElement[] = []) {
+    const coords = this.parseCoords(stone.slot);
+
+    stones.push(stone);
+
+    const up = this.querySelector<GoStoneElement>(
+      `[slot="${coords.col}${Number(coords.row) - 1}"]`
+    );
+
+    if (up && !stones.includes(up)) {
+      stones.push(up);
+
+      this.findGroup(up, stones);
+    }
+
+    const down = this.querySelector<GoStoneElement>(
+      `[slot="${coords.col}${Number(coords.row) + 1}"]`
+    );
+
+    if (down && !stones.includes(down)) {
+      stones.push(down);
+
+      this.findGroup(down, stones);
+    }
+
+    const left = this.querySelector<GoStoneElement>(
+      `[slot="${alphabet[alphabet.indexOf(coords.col) - 1]}${coords.row}"]`
+    );
+
+    if (left && !stones.includes(left)) {
+      stones.push(left);
+
+      this.findGroup(left, stones);
+    }
+
+    const right = this.querySelector<GoStoneElement>(
+      `[slot="${alphabet[alphabet.indexOf(coords.col) + 1]}${coords.row}"]`
+    );
+
+    if (right && !stones.includes(right)) {
+      stones.push(right);
+
+      this.findGroup(right, stones);
+    }
+
+    return stones;
   }
 }
