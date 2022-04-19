678d72dabbffd4befd98b88c941adbd231f07a2e
dblue <dannybluedesign@gmail.com>
2022-04-18 21:35:56 -0400

    cleanup

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe("My First Test", () => {
   it("Does not do much!", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("http://localhost:3000/integration.html");
 
     const moves = [
       "D16",
