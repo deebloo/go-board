415f45717ab3a02d60d17268f8696f604da5ac7e
deebloo <dannybluedesign@gmail.com>
2022-08-19 17:52:18 -0400

    remove things

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,16 +3,7 @@ import minifyHTML from "rollup-plugin-minify-html-literals";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 4200,
-  },
   build: {
-    outDir: "bundled",
-    lib: {
-      entry: "src/register.ts",
-      name: "Go",
-      fileName: (format) => `go.${format}.js`,
-    },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
