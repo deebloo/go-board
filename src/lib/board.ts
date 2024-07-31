4551948ab5f2b4adf947dd6c0d2142d2ca4a0c73
deebloo <deebloo@proton.me>
2024-07-31 11:35:59 -0400

    latest rc

--- src/lib/board.ts
+++ src/lib/board.ts
@@ -1,13 +1,5 @@
 import { inject, injectable } from "@joist/di";
-import {
-  attr,
-  css,
-  html,
-  listen,
-  shadow,
-  tagName,
-  element,
-} from "@joist/element";
+import { attr, css, html, listen, element } from "@joist/element";
 
 import { Debug } from "./debug.js";
 import type { GoStoneElement, StoneColor } from "./stone.js";
@@ -15,200 +7,217 @@ import { findAttachedEnemyStones, findGroup } from "./game.js";
 import { Move, parseSGF } from "./sgf.js";
 import { Sfx } from "./sfx.js";
 
-@injectable
-@element
-export class GoBoardElement extends HTMLElement {
-  @tagName static tag = "go-board";
+@injectable()
+@element({
+  tagName: "go-board",
+  shadow: [
+    css`
+      :host {
+        font-family: system-ui;
+        box-sizing: border-box;
+        background: #fbc467;
+        display: block;
+        padding: 0;
+        position: relative;
+        box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.3);
+        aspect-ratio: 1/1;
+        font-size: clamp(2.5vh, 2.5vw);
+      }
 
+      :is(#header > *, .row > :first-child) {
+        visibility: hidden;
+      }
+
+      :host([coords]) {
+        padding: 1rem 0 0 1rem;
+      }
+
+      :host([coords]) :is(#header > *, .row > *:first-child) {
+        visibility: visible;
+      }
+
+      * {
+        box-sizing: border-box;
+        font-family: inherit;
+      }
+
+      .row {
+        display: flex;
+        width: 100%;
+      }
+
+      .row > * {
+        align-items: center;
+        justify-content: center;
+        border-top: solid 1px #000;
+        border-left: solid 1px #000;
+        display: flex;
+        flex-grow: 1;
+        aspect-ratio: 1/1;
+        position: relative;
+      }
+
+      .row > * > * {
+        position: absolute;
+      }
+
+      #header > * {
+        border-color: transparent;
+        transform: translate(-50%, -10px);
+      }
+
+      .row board-spacer {
+        border-color: transparent;
+        transform: translate(-10px, -50%);
+      }
+
+      .row:last-child slot {
+        border-color: transparent;
+        border-top: solid 1px #000;
+      }
+
+      .row slot:last-child {
+        border-color: transparent;
+        border-left: solid 1px #000;
+      }
+
+      .row:last-child slot:last-child {
+        border-color: transparent;
+      }
+
+      .row slot::slotted(go-marker) {
+        position: absolute;
+        transform: translate(-50%, -50%);
+        padding: 0;
+        height: 98%;
+        width: 98%;
+      }
+
+      .row slot::slotted(go-stone),
+      .row slot button {
+        position: absolute;
+        transform: translate(-50%, -50%);
+        padding: 0;
+        height: 98%;
+        width: 98%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .row slot::slotted(go-stone),
+      .row slot::slotted(go-marker) {
+        z-index: 1000;
+      }
+
+      .row slot.starpoint:after {
+        content: "";
+        display: block;
+        height: 25%;
+        width: 25%;
+        background: #000;
+        border-radius: 50%;
+        position: absolute;
+        transform: translate(-50%, -50%);
+        top: 0;
+        left: 0;
+      }
+
+      :host(:not([disablelastmarker]))
+        .row
+        slot::slotted(go-stone:last-of-type)::after {
+        content: "";
+        height: 50%;
+        width: 50%;
+        border-radius: 50%;
+        border: solid 2px;
+        position: absolute;
+      }
+
+      :host .row slot::slotted(go-stone[color="white"]:last-child)::after {
+        border-color: #000;
+      }
+
+      :host .row slot::slotted(go-stone[color="black"]:last-child)::after {
+        border-color: #fff;
+      }
+
+      .row slot button {
+        border: none;
+        opacity: 0;
+        cursor: pointer;
+        z-index: 1;
+      }
+
+      .row slot button:hover {
+        background: none;
+        opacity: 0.85 !important;
+      }
+
+      .row slot button:after {
+        content: "";
+        display: block;
+        border-radius: 50%;
+        height: 100%;
+        width: 100%;
+      }
+
+      :host([turn="black"]) .row slot button:hover:after {
+        background: #000;
+      }
+
+      :host([turn="white"]) .row slot button:hover:after {
+        background: #fff;
+      }
+
+      :host([debug]) button {
+        opacity: 0.2;
+      }
+
+      :host([readonly]) .row slot button {
+        cursor: default;
+      }
+
+      :host([readonly]) .row slot button:hover:after {
+        background: none !important;
+      }
+    `,
+    html`
+      <div id="header" class="row">
+        <board-spacer></board-spacer>
+      </div>
+    `,
+  ],
+})
+export class GoBoardElement extends HTMLElement {
   static formAssociated = true;
 
-  @shadow styles = css`
-    :host {
-      font-family: system-ui;
-      box-sizing: border-box;
-      background: #fbc467;
-      display: block;
-      padding: 0;
-      position: relative;
-      box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.3);
-      aspect-ratio: 1/1;
-      font-size: clamp(2.5vh, 2.5vw);
-    }
+  @attr()
+  accessor turn: StoneColor = "black";
 
-    :is(#header > *, .row > :first-child) {
-      visibility: hidden;
-    }
+  @attr()
+  accessor src = "";
 
-    :host([coords]) {
-      padding: 1rem 0 0 1rem;
-    }
+  @attr()
+  accessor debug = false;
 
-    :host([coords]) :is(#header > *, .row > *:first-child) {
-      visibility: visible;
-    }
+  @attr()
+  accessor rows = 19;
 
-    * {
-      box-sizing: border-box;
-      font-family: inherit;
-    }
+  @attr()
+  accessor cols = 19;
 
-    .row {
-      display: flex;
-      width: 100%;
-    }
+  @attr()
+  accessor coords = false;
 
-    .row > * {
-      align-items: center;
-      justify-content: center;
-      border-top: solid 1px #000;
-      border-left: solid 1px #000;
-      display: flex;
-      flex-grow: 1;
-      aspect-ratio: 1/1;
-      position: relative;
-    }
+  @attr()
+  accessor readonly = false;
 
-    .row > * > * {
-      position: absolute;
-    }
+  @attr()
+  accessor sfx = "";
 
-    #header > * {
-      border-color: transparent;
-      transform: translate(-50%, -10px);
-    }
-
-    .row board-spacer {
-      border-color: transparent;
-      transform: translate(-10px, -50%);
-    }
-
-    .row:last-child slot {
-      border-color: transparent;
-      border-top: solid 1px #000;
-    }
-
-    .row slot:last-child {
-      border-color: transparent;
-      border-left: solid 1px #000;
-    }
-
-    .row:last-child slot:last-child {
-      border-color: transparent;
-    }
-
-    .row slot::slotted(go-marker) {
-      position: absolute;
-      transform: translate(-50%, -50%);
-      padding: 0;
-      height: 98%;
-      width: 98%;
-    }
-
-    .row slot::slotted(go-stone),
-    .row slot button {
-      position: absolute;
-      transform: translate(-50%, -50%);
-      padding: 0;
-      height: 98%;
-      width: 98%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-
-    .row slot::slotted(go-stone),
-    .row slot::slotted(go-marker) {
-      z-index: 1000;
-    }
-
-    .row slot.starpoint:after {
-      content: "";
-      display: block;
-      height: 25%;
-      width: 25%;
-      background: #000;
-      border-radius: 50%;
-      position: absolute;
-      transform: translate(-50%, -50%);
-      top: 0;
-      left: 0;
-    }
-
-    :host(:not([disablelastmarker]))
-      .row
-      slot::slotted(go-stone:last-of-type)::after {
-      content: "";
-      height: 50%;
-      width: 50%;
-      border-radius: 50%;
-      border: solid 2px;
-      position: absolute;
-    }
-
-    :host .row slot::slotted(go-stone[color="white"]:last-child)::after {
-      border-color: #000;
-    }
-
-    :host .row slot::slotted(go-stone[color="black"]:last-child)::after {
-      border-color: #fff;
-    }
-
-    .row slot button {
-      border: none;
-      opacity: 0;
-      cursor: pointer;
-      z-index: 1;
-    }
-
-    .row slot button:hover {
-      background: none;
-      opacity: 0.85 !important;
-    }
-
-    .row slot button:after {
-      content: "";
-      display: block;
-      border-radius: 50%;
-      height: 100%;
-      width: 100%;
-    }
-
-    :host([turn="black"]) .row slot button:hover:after {
-      background: #000;
-    }
-
-    :host([turn="white"]) .row slot button:hover:after {
-      background: #fff;
-    }
-
-    :host([debug]) button {
-      opacity: 0.2;
-    }
-
-    :host([readonly]) .row slot button {
-      cursor: default;
-    }
-
-    :host([readonly]) .row slot button:hover:after {
-      background: none !important;
-    }
-  `;
-
-  @shadow dom = html`
-    <div id="header" class="row">
-      <board-spacer></board-spacer>
-    </div>
-  `;
-
-  @attr accessor turn: StoneColor = "black";
-  @attr accessor src: string = "";
-  @attr accessor debug = false;
-  @attr accessor rows = 19;
-  @attr accessor cols = 19;
-  @attr accessor coords = false;
-  @attr accessor readonly = false;
-  @attr accessor sfx = "";
-  @attr accessor novalidate = false;
+  @attr()
+  accessor novalidate = false;
 
   moves: Move[] = [];
   sgf: string | null = null;
@@ -246,7 +255,7 @@ export class GoBoardElement extends HTMLElement {
   // when stones are added or removed this map is updated. This holds a reference to all stones on the board and which space they are in.
   // this makes state calculations very cheap. the stone added/removed lifecycle callbacks keep this map in state.
   #spaces = new Map<string, GoStoneElement | null>();
-  #header = this.dom.query("#header")!;
+  #header = this.shadowRoot!.querySelector("#header")!;
   #audio: Sfx | null = null;
   #prevKey = "";
   #currentKey = Array.from({ length: this.rows * this.cols })
@@ -298,7 +307,8 @@ export class GoBoardElement extends HTMLElement {
     this.#spaces.set(stone.slot, null);
   }
 
-  @listen("click") onClick(e: Event) {
+  @listen("click")
+  onClick(e: Event) {
     if (this.readonly) {
       return;
     }
@@ -474,7 +484,7 @@ export class GoBoardElement extends HTMLElement {
         row.append(this.#createSlot(r, c));
       }
 
-      this.dom.shadow.append(row);
+      this.shadowRoot!.append(row);
     }
   }
 
